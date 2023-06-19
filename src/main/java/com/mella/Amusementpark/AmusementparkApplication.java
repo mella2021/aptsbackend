e018898cbb62a488c6f0aea28af0e128378c84a7
mella <mellabae322@gmail.com>
2023-06-19 23:23:37 +0300

    first commit

---
+++ src/main/java/com/mella/Amusementpark/AmusementparkApplication.java
@@ -0,0 +1,13 @@
+package com.mella.Amusementpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmusementparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmusementparkApplication.class, args);
+	}
+
+}
